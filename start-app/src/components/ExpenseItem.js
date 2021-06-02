5db6f7138a928eed2aff36dc720aed611e438696
elleom <el_leo_m@hotmail.com>
2021-06-03 01:37:05 +0200

    added dynamic data

--- start-app/src/components/ExpenseItem.js
+++ start-app/src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+
+    const expensaDate = new Date(2021,2,28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67
+
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expensaDate}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
+                <h2>{expenseTitle}</h2>
             </div>
-            <div className="expense-item__price">Amount</div>
+            <div className="expense-item__price">{expenseAmount}</div>
         </div>
     )
 }
