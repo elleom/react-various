f21fdf11b5f4fab7da68fe82f845622eb8bf92e9
elleom <el_leo_m@hotmail.com>
2021-06-04 00:41:15 +0200

    array dummy-d

--- start-app/src/App.js
+++ start-app/src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem.js'
 
 function App() {
-  return (
-    <div className="App">
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    const expenses = [
+        {title: 'Car Insurance', amount: 294.67, date: new Date(2021, 6, 4).toISOString()},
+        {title: 'Housing Varios', amount: 315.67, date: new Date(2021, 4, 5).toISOString()},
+        {title: 'Rent', amount: 200.67, date: new Date(2021, 3, 6).toISOString()},
+        {title: 'Travel', amount: 100.67, date: new Date(2021, 12, 4).toISOString()}
+    ];
+
+    return (
+        <div className="App">
+            <h2>Let's get started!</h2>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+
+        </div>
+    );
 }
 
 export default App;
--- start-app/src/components/ExpenseItem.js
+++ start-app/src/components/ExpenseItem.js
@@ -1,19 +1,19 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = props => {
 
-    const expensaDate = new Date(2021,2,28);
+    const expenseDate = new Date(2021,2,28);
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 294.67
 
 
     return (
         <div className="expense-item">
-            <div>{expensaDate}</div>
+            <div>{props.date}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
+                <h2>{props.title}</h2>
             </div>
-            <div className="expense-item__price">{expenseAmount}</div>
+            <div className="expense-item__price">{props.amount}</div>
         </div>
     )
 }
