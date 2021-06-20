9abd97d03ba8792e87deda768af26616b7ba13dd
elleom <el_leo_m@hotmail.com>
2021-06-20 17:39:53 +0200

    style

--- start-app/src/components/ExpenseItem.js
+++ start-app/src/components/ExpenseItem.js
@@ -1,20 +1,19 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
-import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem = props => {
-
-
-
+const ExpenseItem = (props) => {
     return (
-        <div className="expense-item">
-            <ExpenseDate date={props.date}></ExpenseDate>
-
-            <div className="expense-item__description">
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
                 <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <div className="expense-item__price">{props.amount}</div>
-        </div>
-    )
+        </Card>
+    );
 }
 
 export default ExpenseItem;
