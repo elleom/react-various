29ee800da924acd816725e93d25eb02e804000e3
elleom <el_leo_m@hotmail.com>
2021-06-20 17:49:47 +0200

    fix style issue

--- start-app/src/App.js
+++ start-app/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import Expenses from "./components/Expenses";
+import React from 'react';
 
-function App() {
+import Expenses from './components/Expenses/Expenses';
 
+const App = () => {
     const expenses = [
         {
             id: 'e1',
@@ -10,7 +10,7 @@ function App() {
             amount: 94.12,
             date: new Date(2020, 7, 14),
         },
-        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
+        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
         {
             id: 'e3',
             title: 'Car Insurance',
@@ -25,11 +25,17 @@ function App() {
         },
     ];
 
+    // return React.createElement(
+    //   'div',
+    //   {},
+    //   React.createElement('h2', {}, "Let's get started!"),
+    //   React.createElement(Expenses, { items: expenses })
+    // );
 
     return (
-        <div className="App">
+        <div>
             <h2>Let's get started!</h2>
-            <Expenses items={expenses}/>
+            <Expenses items={expenses} />
         </div>
     );
 }
